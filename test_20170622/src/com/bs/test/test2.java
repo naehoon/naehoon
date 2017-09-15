41d4ff6a4292dcfc8b6740902422d1fa38f2b542
BS <BS@BS-PC>
2017-09-15 17:09:20 +0900

    ex

---
+++ test_20170622/src/com/bs/test/test2.java
@@ -0,0 +1,16 @@
+package com.bs.test;
+
+public class test2 {
+	
+	
+	
+	public static void main(String[]args){
+		
+		double  d  = Math.random();
+		int i = (int)(d*45);
+		
+		System.out.println(d);
+		System.out.println(i);
+		System.out.println("キーボードもらってもいいですか。");
+	}
+}
